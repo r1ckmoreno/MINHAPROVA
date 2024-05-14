40f42f152aec97d7ba4d353bf86e334f99a5d00f
Henrique <125594347+r1ckmoreno@users.noreply.github.com>
2024-05-14 09:17:54 -0300

    VotacaoBigBrotherBrasil.java

--- BigBrotherBrasil/src/VotacaoBigBrotherBrasil.java
+++ BigBrotherBrasil/src/VotacaoBigBrotherBrasil.java
@@ -36,7 +36,7 @@ public class VotacaoBigBrotherBrasil {
         Scanner scanner = new Scanner(System.in);
         String voto;
         do {
-            System.out.print("Em quem você vota para sair da casa? ");
+            System.out.print("Em quem você vota para sair da casa? (digite sair para apurar resultados) ");
             voto = scanner.nextLine();
             if (!voto.equalsIgnoreCase("sair")) {
                 boolean encontrado = false;
@@ -53,17 +53,68 @@ public class VotacaoBigBrotherBrasil {
             }
         } while (!voto.equalsIgnoreCase("sair"));
 
-        Jogador eliminado = jogadores.get(0);
+        ArrayList<Jogador> eliminados = new ArrayList<>();
+        int maiorNumeroVotos = -1;
+
         for (Jogador jogador : jogadores) {
-            if (jogador.getVotos() > eliminado.getVotos()) {
-                eliminado = jogador;
+            int votos = jogador.getVotos();
+            if (votos > maiorNumeroVotos) {
+                eliminados.clear();
+                eliminados.add(jogador);
+                maiorNumeroVotos = votos;
+            } else if (votos == maiorNumeroVotos) {
+                eliminados.add(jogador);
             }
         }
 
-        System.out.println("Se eu conseguir mover montanhas, se eu conseguir surfar um tsunami,");
-        System.out.println("se eu conseguir domar o sol, se eu conseguir fazer o mar virar sertão,");
-        System.out.println("e o sertão virar mar, se eu conseguir dizer o que eu nunca vou conseguir dizer,");
-        System.out.println("aí terá chegado o dia em que eu vou conseguir te eliminar com alegria.");
-        System.out.println("Com " + eliminado.getVotos() + " votos, é você quem sai " + eliminado.getNome());
+        if (eliminados.size() == 1) {
+            Jogador eliminado = eliminados.get(0);
+            System.out.println("Se eu conseguir mover montanhas, se eu conseguir surfar um tsunami,");
+            System.out.println("se eu conseguir domar o sol, se eu conseguir fazer o mar virar sertão,");
+            System.out.println("e o sertão virar mar, se eu conseguir dizer o que eu nunca vou conseguir dizer,");
+            System.out.println("aí terá chegado o dia em que eu vou conseguir te eliminar com alegria.");
+            System.out.println("Com " + eliminado.getVotos() + " votos, é você quem sai " + eliminado.getNome());
+        } else {
+            System.out.println("Empate entre os jogadores com " + maiorNumeroVotos + " votos. Realize uma nova votação para desempate.");
+
+            for (Jogador jogador : eliminados) {
+                System.out.println("Votar em " + jogador.getNome());
+            }
+
+            String novoVoto;
+            Jogador eliminadoDesempate = eliminados.get(0);
+            boolean votoComputado = false;
+            do {
+                System.out.print("Em quem você vota para sair da casa no desempate? (após digitar o nome, digite sair para apurar resultado) ");
+                novoVoto = scanner.nextLine();
+                if (!novoVoto.equalsIgnoreCase("sair")) {
+                    boolean encontrado = false;
+                    for (Jogador jogador : eliminados) {
+                        if (jogador.getNome().equalsIgnoreCase(novoVoto)) {
+                            if (!votoComputado) {
+                                jogador.incrementaUmVoto();
+                                votoComputado = true;
+                                if (jogador.getVotos() > eliminadoDesempate.getVotos()) {
+                                    eliminadoDesempate = jogador;
+                                }
+                            } else {
+                                System.out.println("Você só pode votar uma vez.");
+                            }
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("Jogador não encontrado na lista de desempate.");
+                    }
+                }
+            } while (!novoVoto.equalsIgnoreCase("sair"));
+
+            System.out.println("Se eu conseguir mover montanhas, se eu conseguir surfar um tsunami,");
+            System.out.println("se eu conseguir domar o sol, se eu conseguir fazer o mar virar sertão,");
+            System.out.println("e o sertão virar mar, se eu conseguir dizer o que eu nunca vou conseguir dizer,");
+            System.out.println("aí terá chegado o dia em que eu vou conseguir te eliminar com alegria.");
+            System.out.println("Com " + eliminadoDesempate.getVotos() + " votos no desempate, é você quem sai " + eliminadoDesempate.getNome());
+        }
     }
 }
